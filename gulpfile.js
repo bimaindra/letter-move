b61795b95985734be24de83f2a14f4a47563dc85
wongdarjo <bimaindramulya@gmail.com>
2021-03-20 15:31:57 +0700

    add development config

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+
+//--- BROWSER SYNC INIT
+gulp.task('browser-sync', (done) => {
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+        },
+    })
+    done()
+})
+
+//--- BROWSER SYNC RELOAD
+gulp.task('browser-reload', (done) => {
+    browserSync.reload()
+    done()
+})
+
+//--- MOVE IMAGE FILES
+gulp.task('image', () => {
+    return gulp
+        .src(['./assets/images/**/*.*'])
+        .pipe(gulp.dest('./dist/assets/images'))
+})
+
+//--- MOVE HTML FILES
+gulp.task('html', () => {
+    return gulp.src(['./public/*.html']).pipe(gulp.dest('./dist'))
+})
+
+//--- SERVING FILES
+gulp.task(
+    'serve',
+    gulp.series('html', 'image', 'browser-sync', () => {
+        gulp.watch(
+            ['./public/*.html'],
+            gulp.series('html', 'image', 'browser-reload')
+        )
+    })
+)
+
+//--- DEFAULT TASK
+gulp.task('default', gulp.series('serve'))
