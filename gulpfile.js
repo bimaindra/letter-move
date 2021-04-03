226a5f48b060e46999de7b690a91393d3acb260d
wongdarjo <bimaindramulya@gmail.com>
2021-04-03 12:55:22 +0700

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,10 @@ const gulpWrap = require('gulp-wrap');
 const { watch } = require('rollup');
 const browserSync = require('browser-sync').create();
 
+
 //-- ENV
 const isDebug = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !== 'production');
+
 
 //--- DIRECTORY MAPPING
 const root = {
@@ -26,18 +28,18 @@ const root = {
 
 const dir = {
     source: {
-        public: root.src + '/public',
-        css: root.assets + '/css',
-        js: root.assets + '/js',
-        images: root.assets + '/images',
-        fonts: root.assets + '/fonts',
+        public: `${root.src}/public`,
+        css: `${root.assets}/css`,
+        js: `${root.assets}/js`,
+        images: `${root.assets}/images`,
+        fonts: `${root.assets}/fonts`,
     },
     build: {
-        base: root.build,
-        css: root.build + '/assets/css',
-        js: root.build + '/assets/js',
-        images: root.build + '/assets/images',
-        fonts: root.build + '/assets/fonts',
+        base: `${root.build}`,
+        css: `${root.build}/assets/css`,
+        js: `${root.build}/assets/js`,
+        images: `${root.build}/assets/images`,
+        fonts: `${root.build}/assets/fonts`,
     },
 };
 
@@ -46,7 +48,7 @@ const dir = {
 gulp.task('browser-sync', (done) => {
     browserSync.init({
         server: {
-            baseDir: root.build,
+            baseDir: dir.build.base,
         },
     });
     done();
@@ -64,7 +66,7 @@ gulp.task('browser-reload', (done) => {
 gulp.task('compile-hbs', () => {
     // Assume all partials start with an underscore
     // You could also put them in a folder such as source/templates/partials/*.hbs
-    let partials = gulp.src([dir.source.public + '/partials/**/*.hbs'])
+    let partials = gulp.src([`${dir.source.public}/partials/**/*.hbs`])
       .pipe(handlebars())
       .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
         imports: {
@@ -76,7 +78,7 @@ gulp.task('compile-hbs', () => {
         }
       }));
   
-    let templates = gulp.src(dir.source.public + '/templates/**/*.hbs')
+    let templates = gulp.src(`${dir.source.public}/templates/**/*.hbs`)
       .pipe(handlebars())
       .pipe(wrap('Handlebars.template(<%= contents %>)'))
       .pipe(declare({
@@ -94,14 +96,14 @@ gulp.task('compile-hbs', () => {
 //-- COPY JS HBS
 gulp.task('copy-js-hbs', () => {
     return gulp
-        .src(root.npm + '/handlebars/dist/handlebars.runtime.js')
+        .src(`${root.npm}/handlebars/dist/handlebars.runtime.js`)
         .pipe(gulp.dest(dir.build.js));
 });
 
 
 //-- COPY HTML
 gulp.task('copy-html', () => {
-    return gulp.src(dir.source.public + '/**/*.html').pipe(gulp.dest(dir.build.base));
+    return gulp.src(`${dir.source.public}/**/*.html`).pipe(gulp.dest(dir.build.base));
 });
 
 
@@ -112,7 +114,7 @@ gulp.task('compile', gulp.series(gulp.parallel('copy-js-hbs', 'copy-html'), 'com
 //--- SERVING FILES
 gulp.task('serve', gulp.series('compile', 'browser-sync', () => {
         gulp.watch(
-            [dir.source.public + '/*.html', dir.source.public + '/**/*.hbs'],
+            [`${dir.source.public}/*.html`, `${dir.source.public}/**/*.hbs`],
             gulp.series('compile', 'browser-reload')
         );
     })
